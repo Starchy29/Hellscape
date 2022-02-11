01675094f7b81a9d02b62238b0ee45d83c6a98c1
Starchy29 <ethanball29@gmail.com>
2022-02-11 14:49:26 -0500

    Slowed player max speed and added variables to crumbling walls

--- Hellscape/Assets/Scripts/CollapsingWallScript.cs
+++ Hellscape/Assets/Scripts/CollapsingWallScript.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class CollapsingWallScript : MonoBehaviour
 {
     private float timer = 0f;
+    [SerializeField] private float respawnTime = 5f;
+    [SerializeField] private float crumbleTime = 2f;
 
     private void Update()
     {
         if(timer > 0) {
             timer -= Time.deltaTime;
             if(timer <= 0) {
-                timer = -5f; // respawn time
+                timer = -respawnTime;
                 gameObject.GetComponent<BoxCollider2D>().enabled = false;
                 gameObject.GetComponent<SpriteRenderer>().enabled = false;
             }
@@ -28,6 +30,6 @@ public class CollapsingWallScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        timer = 2f; // time after collision before destruction
+        timer = crumbleTime;
     }
 }
